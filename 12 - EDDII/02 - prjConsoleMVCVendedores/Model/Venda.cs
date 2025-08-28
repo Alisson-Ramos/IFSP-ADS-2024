f93eb8a78884faec15c12e4e3157f46eb80e93b3
Alisson Ramos <81827023+Alisson-Ramos@users.noreply.github.com>
2025-08-28 17:02:43 -0300

    Add files via upload

---
+++ 12 - EDDII/02 - prjConsoleMVCVendedores/Model/Venda.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ProjetoVendedores.MVC.Model
+{
+    /// <summary>
+    /// Representa uma venda realizada por um vendedor,
+    /// contendo quantidade e valor total.
+    /// </summary>
+    public class Venda
+    {
+        #region Propriedades Privadas
+        private int _qtde;
+        private double _valor;
+        #endregion
+
+        #region Propriedades Públicas
+        /// <summary>
+        /// Quantidade de itens vendidos.
+        /// </summary>
+        public int Qtde
+        {
+            get { return _qtde; }
+            set { if (value > 0) _qtde = value; }
+        }
+
+        /// <summary>
+        /// Valor total da venda.
+        /// </summary>
+        public double Valor
+        {
+            get { return _valor; }
+            set { if (value > 0) _valor = value; }
+        }
+        #endregion
+
+        #region Construtores
+        /// <summary>
+        /// Cria uma venda com quantidade e valor total.
+        /// </summary>
+        /// <param name="qtde">Quantidade de itens vendidos.</param>
+        /// <param name="valor">Valor total da venda.</param>
+        public Venda(int qtde, double valor)
+        {
+            Qtde = qtde;
+            Valor = valor;
+        }
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Calcula o valor médio por item vendido.
+        /// </summary>
+        /// <returns>Valor médio unitário da venda.</returns>
+        public double ValorMedio()
+        {
+            if (Qtde == 0)
+                return 0;
+            return Valor / Qtde;
+        }
+        #endregion
+    }
+}
---
+++ 12 - EDDII/02 - prjConsoleMVCVendedores/Program.cs
@@ -0,0 +1,12 @@
+﻿using ProjetoVendedores.MVC.View;
+
+namespace ProjetoVendedores.MVC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new VendedoresView().Init();
+        }
+    }
+}
